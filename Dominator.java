6efb8534cccdcccf3c99ef215b22b3abce312e44
Roger M <roger.kang.mo@gmail.com>
2012-10-06 00:10:32 -0400

    Added in a little fix. Updated info and added code per new info.

--- Dominator.java
+++ Dominator.java
@@ -4,6 +4,10 @@
 * 12 would be the dominator because it occupies 3 of 7 indeces. That's more than any other number does.
 *
 * Basically, we're finding the mode... but Dominator sounds cooler
+* 
+* Update: I may have misread the problem. It might be the case that for a number to dominate,
+* it must be in more than have of the indeces of the array. So, over half of the array would have to be that number.
+* It's a small change. Aaaaaaaaaaand done.
 */
 
 import java.util.*;
@@ -11,7 +15,11 @@ import java.util.*;
 public class Dominator{
 	
 	public static void main(String _[]){
-		int[] nums = new int[]{12, 85, 39, 92, 12, 12, 92};
+		int[] nums = new int[]{12, 85, 39, 92, 12, 12, 92, 12};
+		printNums(nums);
+		System.out.println("Dominator is " + findDominator(nums));
+
+		nums = new int[]{12, 85, 39, 92, 12, 12, 92, 49, 81, 72};
 		printNums(nums);
 		System.out.println("Dominator is " + findDominator(nums));
 	}
@@ -25,11 +33,13 @@ public class Dominator{
 			if(numsMap.get(nums[i]) == null){
 				numsMap.put(nums[i], 1);
 			}else{
-				numsMap.put(nums[i], numsMap.get(nums[i]));
+				numsMap.put(nums[i], numsMap.get(nums[i])+1);
 				if(numsMap.get(nums[i]) > highCount)
 					retVal = nums[i];
 			}
 		}
+
+		if(numsMap.get(retVal) < nums.length/2) retVal = -1;		
 
 		return retVal;
 	}
@@ -39,5 +49,6 @@ public class Dominator{
 		for(int i = 0, n = nums.length; i < n; i++){
 			System.out.print(nums[i]+" ");
 		}
+		System.out.println("");
 	}
 }
