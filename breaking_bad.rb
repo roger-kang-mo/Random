a6849562cf795d7e73744b0a02fe5cb7fdafe656
Roger Mo <roger2016@gmail.com>
2013-08-05 14:54:49 -0700

    Breaking Bad text badifier with spec

--- breaking_bad.rb
+++ breaking_bad.rb
@@ -1,3 +1,11 @@
+# This emulates the credits on Breaking Bad where they show text with
+# elements in place of letters.
+# E.g. Breaking Bad 			[Br]eaking Bad
+# 		Created by 				  Crea[Te]d by
+# 		Vince Gilligan  		  Vin[Ce] Gilligan
+#
+
+
 module BreakingBad
 	SYMBOLS = ["Ac","Ag","Al","Am","Ar","As","At","Au","B","Ba","Be","Bh","Bi",
        "Bk","Br","C","Ca","Cd","Ce","Cf","Cl","Cm","Cn","Co","Cr","Cs",
@@ -18,23 +26,62 @@ end
 
 class BreakingBadalyzer
 
-	attr_reader :text, :lines, :words
+	attr_reader :text, :lines, :words, :last_badified_text
 
 	def initialize(opts = {})
 		@text = opts[:text] ? opts[:text] : BreakingBad::DEFAULT_TEXT
 		@words = Hash.new
 		@lines = Array.new
+
+		breaking_badify
 	end
 
+	def generate_badified
+		output = @text.dup
+
+		@lines.each do |line|
+			words_in_line = line.split()
+			not_replaced = true
+			while not_replaced
+				word_to_replace = words_in_line[rand(words_in_line.length)]
+				if @words[word_to_replace]
+					random_combo = @words[word_to_replace][rand(@words[word_to_replace].length)]
+					output[word_to_replace] = random_combo
+					not_replaced = false
+				end
+			end
+		end
+		@last_badified_text = output
+	end
+
+	private
 	def breaking_badify
 		@lines = @text.split(/\r?\n/)
-		
+
+		get_combinations = Proc.new do |word|
+			combos = Array.new
+			BreakingBad::SYMBOLS.each do |elem|
+				word_downcase = word.downcase
+				elem_downcase = elem.downcase
+
+				if word_downcase.include? elem_downcase
+					end_index = word_downcase.index(elem_downcase) + elem.length
+					new_combo = word.dup
+					new_combo [word_downcase.index(elem_downcase)...end_index]  = "[#{elem}]" 
+					combos << new_combo
+				end
+			end
+
+			[word, combos]
+		end
+
 		@lines.each do |line|
 			line = line.split()
-			line_hash = Hash[line.map {|x| [x, Array.new]}]
+
+			line_hash = Hash[line.map { |word| get_combinations.call(word)}]
+
 			@words = BreakingBad::merge_recursively(@words, line_hash)
 		end
-		
 	end
 end
 
--- spec/breaking_bad_spec.rb
+++ spec/breaking_bad_spec.rb
@@ -11,15 +11,30 @@ describe BreakingBad do
 		it { should_not be_nil }
 
 		specify { subject.text.should_not be_nil }
+		specify { subject.text.should == "Breaking\n    Bad\nCreated by\n   Vince Gilligan"}
 	end
 
-	context 'given default text' do
+	context 'with custom text' do
 		subject { BreakingBadalyzer.new({ text: "Excess like this that's\ndestroying the plastic rainforest."})}
 		before do
-			subject.breaking_badify
+			subject.generate_badified
 		end
 		
 		specify { subject.text.should == "Excess like this that's\ndestroying the plastic rainforest." }
 		specify { subject.lines.length.should == 2 }
+
+		it 'should generate a new badified text' do
+			current_badified = subject.last_badified_text
+			subject.generate_badified
+			current_badified.should_not eq(subject.last_badified_text)
+		end
+
+		it 'should only replace one set of characters in a line' do
+			lines = subject.last_badified_text.split(/\r?\n/)
+			num_brackets = 0
+			lines.each { |line| num_brackets += line.count('[') }
+
+			lines.length.should == num_brackets
+		end
 	end
 end
